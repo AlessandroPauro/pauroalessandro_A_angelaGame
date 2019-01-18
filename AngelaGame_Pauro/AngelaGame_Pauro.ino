564181487c1c65c7a1b13dbae1e86fdddf5b7712
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-01-18 09:39:23 +0100

    18/07/2019 9:39

--- AngelaGame_Pauro/AngelaGame_Pauro.ino
+++ AngelaGame_Pauro/AngelaGame_Pauro.ino
@@ -3,6 +3,8 @@ void setup() {
   int Giocatore = 0;
   int PunteggioTOT = 0;
   int Meta = 30;  
+  int Giocate[] = {1, 2, 3, 4, 5, 6};
+  int Giocata = 1;
 }
   void AumentaMeta()
 {
@@ -14,6 +16,21 @@ void setup() {
  if(Meta == 30 ){Serial.print("Non puoi diminuire la Meta");}
  else{Meta--;}
 }
+
+void ValoreSelezionabile()
+{
+  switch (Giocata)
+  {
+    case 1 || 6: 
+    Giocate [] = {2, 3, 4, 5};
+
+    case 2 || 5:
+    Giocate [] = {1, 3, 4, 6};
+    
+    case 3 || 4:
+    Giocate [] = {1, 2, 5, 6};   
+  } 
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
