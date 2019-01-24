3a818c39ba64584118e1b44700a3215b604dc08f
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-01-24 19:07:27 +0100

    24/01/2019

--- AngelaGame_Pauro/AngelaGame_Pauro.ino
+++ AngelaGame_Pauro/AngelaGame_Pauro.ino
@@ -1,38 +1,40 @@
-  int Giocatore = 0;
-  int PunteggioTOT = 0;
-  int Meta = 30;  
-  int Giocate[] = {1, 2, 3, 4, 5, 6};
-  int Giocata = 1;
+int Giocatore = 0;
+int PunteggioTOT = 0;
+int Meta = 30;
+int Giocata = 1;
+int PrimaGiocata[7] = {0, 1, 2, 3, 4, 5, 6};
+int Giocate[4];
+
 void setup() {
   // put your setup code here, to run once
 }
-  void AumentaMeta()
+void AumentaMeta()
 {
- if(Meta == 99){Serial.print("Non puoi aumentare la Meta");}
- else{Meta++;}
-}
-  void DiminuisciMeta()
-{
- if(Meta == 30 ){Serial.print("Non puoi diminuire la Meta");}
- else{Meta--;}
-}
-
-void ValoreSelezionabile()
-{
-  switch (Giocata)
-  {
-    case 1 || 6: 
-    Giocate [6] = {2, 3, 4, 5};
-
-    case 2 || 5:
-    Giocate [6] = {1, 3, 4, 6};
-    
-    case 3 || 4:
-    Giocate [6] = {1, 2, 5, 6};   
-  } 
+  if (Meta == 99) {
+    Serial.print("Non puoi aumentare la Meta");
   }
+  else {
+    Meta++;
+  }
+}
+void DiminuisciMeta()
+{
+  if (Meta == 30 ) {
+    Serial.print("Non puoi diminuire la Meta");
+  }
+  else {
+    Meta--;
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  ValoreSelezionabile();
+}
+
+  void  ValoreSelezionabile(){
+    switch (Giocata){
+       case 0: Giocatore = 1;
+
+   }
 }
