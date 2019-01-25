48b07337adbed9f7c90b3ca4484e98c526811fb3
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-01-25 09:02:01 +0100

    25/01/2019 Implementazione metodi: ValoreSel,Vittoria

--- AngelaGame_Pauro/AngelaGame_Pauro.ino
+++ AngelaGame_Pauro/AngelaGame_Pauro.ino
@@ -1,40 +1,50 @@
 int Giocatore = 0;
 int PunteggioTOT = 0;
-int Meta = 30;
-int Giocata = 1;
-int PrimaGiocata[7] = {0, 1, 2, 3, 4, 5, 6};
-int Giocate[4];
+int Meta;
+int Giocata;
+int Caso00 [6] = {1, 2, 3, 4, 5, 6};
+int Caso16 [4] = {2, 3, 4, 5};
+int Caso25 [4] = {1, 3, 4, 6};
+int Caso34 [4] = {1, 2, 5, 6};
 
 void setup() {
-  // put your setup code here, to run once
 }
-void AumentaMeta()
+void MetaSelezionabile()
 {
-  if (Meta == 99) {
-    Serial.print("Non puoi aumentare la Meta");
-  }
-  else {
-    Meta++;
-  }
+  Meta = Serial.parseInt();
+  if (Meta > 99) {
+    Serial.print("Meta non disponibile, MetaMAX 99");
+    Meta = 99;
+  } 
+  
+  else if (Meta < 30 ) {
+    Serial.print("Meta non disponibile, MetaMIN 30");
+    Meta = 30;
+  }  
 }
-void DiminuisciMeta()
+
+
+void loop() { 
+  while(Serial.available() == 0){}
+  if (Giocata > 6){Serial.print("Seleziona un numero da 0 a 6");}
+  else{ValoreSelezionabile();}
+}
+   void  ValoreSelezionabile(){
+   Giocata = Serial.parseInt();   
+   switch (Giocata){
+       case 0: Caso00;  break;
+       case 1: Caso16;  break;
+       case 2: Caso25;  break;
+       case 3: Caso34;  break;
+       case 4: Caso34;  break;
+       case 5: Caso25;  break;
+       case 6: Caso16;  break;
+    }
+}
+
+
+void FinePartita()
 {
-  if (Meta == 30 ) {
-    Serial.print("Non puoi diminuire la Meta");
-  }
-  else {
-    Meta--;
-  }
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  ValoreSelezionabile();
-}
-
-  void  ValoreSelezionabile(){
-    switch (Giocata){
-       case 0: Giocatore = 1;
-
-   }
+  if (PunteggioTOT == Meta){Serial.print("Ha vinto il giocatore " + Giocatore)}
+  else if (PunteggioTOT > Meta){Serial.print("Ha perso il giocatore " + Giocatore)}
 }
