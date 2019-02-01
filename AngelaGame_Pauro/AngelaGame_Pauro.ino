7129bfac3b37880d9ce9af904f90e19de31c9b69
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-02-01 10:14:01 +0100

    01/02/2019 Completamento con piccoli errori

--- AngelaGame_Pauro/AngelaGame_Pauro.ino
+++ AngelaGame_Pauro/AngelaGame_Pauro.ino
@@ -1,50 +1,141 @@
 int Giocatore = 0;
 int PunteggioTOT = 0;
+int Stato = 1;
 int Meta;
 int Giocata;
-int Caso00 [6] = {1, 2, 3, 4, 5, 6};
+int Caso00 [7] = {0, 1, 2, 3, 4, 5, 6};
 int Caso16 [4] = {2, 3, 4, 5};
 int Caso25 [4] = {1, 3, 4, 6};
 int Caso34 [4] = {1, 2, 5, 6};
 
 void setup() {
+  Serial.begin(9600);
 }
 void MetaSelezionabile()
-{
-  Meta = Serial.parseInt();
-  if (Meta > 99) {
-    Serial.print("Meta non disponibile, MetaMAX 99");
-    Meta = 99;
-  } 
-  
-  else if (Meta < 30 ) {
-    Serial.print("Meta non disponibile, MetaMIN 30");
-    Meta = 30;
-  }  
-}
-
-
-void loop() { 
-  while(Serial.available() == 0){}
-  if (Giocata > 6){Serial.print("Seleziona un numero da 0 a 6");}
-  else{ValoreSelezionabile();}
-}
-   void  ValoreSelezionabile(){
-   Giocata = Serial.parseInt();   
-   switch (Giocata){
-       case 0: Caso00;  for(int i=0; i<= 6 ; i++){if (Giocata != Caso00 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 1: Caso16;  for(int i=0; i<= 5 ; i++){if (Giocata != Caso16 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 2: Caso25;  for(int i=0; i<= 6 ; i++){if (Giocata != Caso25 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 3: Caso34;  for(int i=0; i<= 6 ; i++){if (Giocata != Caso34 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 4: Caso34;  for(int i=0; i<= 6 ; i++){if (Giocata != Caso34 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 5: Caso25;  for(int i=0; i<= 6 ; i++){if (Giocata != Caso25 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
-       case 6: Caso16;  for(int i=0; i<= 5 ; i++){if (Giocata != Caso16 [i]){Serial.print("Giocata non valida");}else{Serial.print(Giocata), Giocatore++;}}break;
+{ if (Meta == 0) {
+    Serial.println("Inserisci valore tra 30 e 99");
+    while (Serial.available() == 0) {}
+    Meta = Serial.parseInt();
+    if (Meta > 99) {
+      Serial.println("Meta non disponibile, MetaMAX 99");
+      Meta = 0;
     }
+
+    else if (Meta < 30 ) {
+      while (Serial.available() == 0) {}
+      Serial.println("Meta non disponibile, MetaMIN 30");
+      Meta = 0;
+    }
+
+    if (Meta > 30 && Meta < 99) {
+      Stato = 2;
+    }
+  }
 }
-  void 
+
+
+
+void loop() {
+  if (Stato == 1) {
+    MetaSelezionabile();
+  } else if (Stato == 2) {
+    Gioco();
+  } else if (Stato == 3) {
+    FinePartita();
+  }
+}
+void Gioco() {
+  Serial.println("Seleziona un numero da 0 a 6");
+  while (Serial.available() == 0) {}
+  if (Giocata < 6 && Giocatore == 0) {
+    ValoreSelezionabile();
+  }
+  else {}
+}
+void ValoreSelezionabile() {
+  Giocata = Serial.parseInt();
+  if (Giocata == 0) {
+    for (int i = 0; i < 7 ; i++) {
+      if (Giocata != Caso00 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 1) {
+    for (int i = 0; i <= 5 ; i++) {
+      if (Giocata != Caso16 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 2) {
+    for (int i = 0; i <= 6 ; i++) {
+      if (Giocata != Caso25 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 3) {
+    for (int i = 0; i <= 6 ; i++) {
+      if (Giocata != Caso34 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 4) {
+    for (int i = 0; i <= 6 ; i++) {
+      if (Giocata != Caso34 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 5) {
+    for (int i = 0; i <= 6 ; i++) {
+      if (Giocata != Caso25 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Giocata == 6) {
+    for (int i = 0; i <= 5 ; i++) {
+      if (Giocata != Caso16 [i]) {
+        Serial.println("Giocata non valida");
+      } else {
+        Serial.println(Giocata);
+        Giocatore++, PunteggioTOT = PunteggioTOT + Giocata;
+      }
+    }
+  }
+  if (Meta == PunteggioTOT || Meta > PunteggioTOT) {
+    Stato = 3;
+  }
+}
 
 void FinePartita()
 {
-  if (PunteggioTOT == Meta){Serial.print("Ha vinto il giocatore " + if();}
-  else if (PunteggioTOT > Meta){Serial.print("Ha perso il giocatore " + Giocatore);}
+  if (PunteggioTOT == Meta) {
+    Serial.println("Ha vinto il giocatore " + (Giocatore % 2 == 0) ? "Giocatore1" : "Giocatore2");
+  }
+  else if (PunteggioTOT > Meta) {
+    Serial.println("Ha perso il giocatore " + (Giocatore % 2 == 0) ? "Giocatore1" : "Giocatore2");
+  }
+  Stato = 1;
 }
