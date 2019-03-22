e1572f6f71894aa5aaed51bb57e6c7a6e491a688
Alessandro <alessandro.pauro@barsanti.edu.it>
2019-03-22 09:55:18 +0100

    elezione giocata player2

--- AngelaGame_Pauro/AngelaGame_Pauro.ino
+++ AngelaGame_Pauro/AngelaGame_Pauro.ino
@@ -14,25 +14,23 @@ int Caso17 [6] = {1, 2, 3, 4, 5, 6};
 int Caso16 [4] = {2, 3, 4, 5};
 int Caso25 [4] = {1, 3, 4, 6};
 int Caso34 [4] = {1, 2, 5, 6};
-/*
-const int buttonPin1 = 7;
-const int buttonPin2 = 8;
-const int buttonPin3 = 9;*/
+
+const int buttonPin1 = 22;
+const int buttonPin2 = 24;
+const int buttonPin3 = 26;
 
    
 void setup() {
   lcd.begin(16, 2);
-  Serial.begin(9600);
-   pinMode(22, INPUT_PULLUP);
-   pinMode(24, INPUT_PULLUP);
-   pinMode(26, INPUT_PULLUP);
+   pinMode(buttonPin1, INPUT_PULLUP);
+   pinMode(buttonPin2, INPUT_PULLUP);
+   pinMode(buttonPin3, INPUT_PULLUP);
 }
 
 
 
 void MetaSelezionabile()
 {   
-  
     lcd.print("Inserisci valore");
     lcd.setCursor(2,1);
     lcd.print("tra 30 e 99");
@@ -41,85 +39,101 @@ void MetaSelezionabile()
     lcd.print(MetaIn);
     delay(3000);
     lcd.clear();
-    while (24 != LOW)
-     {
-      lcd.print(MetaIn);  
-      delay(100);
-      if(22 == LOW){MetaIn++;}
-      if(26 == LOW){MetaIn--;}
-      lcd.clear();
-      Serial.print(digitalRead(22));
-     }
-  lcd.print("fuori"); 
-   if(24 == LOW)
-    {
-      ContolloMeta();
-    }
-
+    SelezionaConBottone();
+    ControlloMeta();
   }
 
-void ContolloMeta(){    
-  if(MetaIn < 30){lcd.print("Valore troppo basso");MetaIn = 30;}
-  else{if(MetaIn>99){lcd.print("Valore troppo alto");MetaIn = 99;}}
-  if(MetaIn >30 && MetaIn <99){Stato = 2;}} 
+void SelezionaConBottone(){    while (digitalRead(buttonPin2) != LOW)
+     {
+      lcd.print(MetaIn); 
+      delay(100); 
+      if(digitalRead(buttonPin1) == LOW){MetaIn++;}
+      if(digitalRead(buttonPin3) == LOW){MetaIn--;}
+      lcd.clear();
+     }}
 
+
+void ControlloMeta(){    
+  if(MetaIn < 30){lcd.print("Valore troppo ");  lcd.setCursor(2,1);
+    lcd.print("basso");delay(3000);lcd.clear();}
+  else if(MetaIn>99){lcd.print("Valore troppo "); lcd.setCursor(2,1);
+    lcd.print("alto");delay(3000);lcd.clear();}
+  if(MetaIn >= 30 && MetaIn <= 99){lcd.print("FATTO");delay(2000);lcd.clear();Stato=2; MetaIn=Meta;}} 
 
 
 void Giocata0(){
+  Giocata = MetaIn;
+  SelezionaConBottone();
+  MetaIn = Giocata;
+  lcd.print(Giocata);delay(3000);
     for (int i = 0; i < 7 ; i++) {
-      if (Giocata == Caso00 [i]) {lcd.print(Giocata);
+      if (Giocata == Caso00 [i]) {lcd.print(Giocata);lcd.clear();
       PunteggioTOT = PunteggioTOT + Giocata; 
          backup = Giocata;
-        lcd.print("Punteggio: "+ PunteggioTOT);
+        lcd.print("Punteggio: "); lcd.setCursor(2,1);lcd.print(PunteggioTOT);delay(2000);lcd.clear();
       }
       }
     }
     
 void Giocata17(){
+  Giocata = MetaIn;
+  SelezionaConBottone();
+  MetaIn = Giocata;
+  lcd.print(Giocata);delay(3000);
     for (int i = 0; i <= 6 ; i++) {
       if (Giocata == Caso17 [i]) {
        lcd.print(Giocata);
         backup = Giocata;
         PunteggioTOT = PunteggioTOT + Giocata; 
-        lcd.print("Punteggio: ") + PunteggioTOT;
+        lcd.print("Punteggio: ");lcd.setCursor(2,1);lcd.print(PunteggioTOT);delay(2000);lcd.clear();
       }
     }
   }
 
 void Giocata1(){
+  Giocata = MetaIn;
+  SelezionaConBottone();
+  MetaIn = Giocata;
+  lcd.print(Giocata);delay(3000);
     for (int i = 0; i <= 4 ; i++) {
       if (Giocata != Caso16 [i]) {
-        lcd.print("Giocata non valida");
+        lcd.print("Giocata non valida");delay(2000);lcd.clear();
       } else {
        lcd.print(Giocata);
         backup = Giocata;
         PunteggioTOT = PunteggioTOT + Giocata; 
-       lcd.print("Punteggio: ") + PunteggioTOT;
+       lcd.print("Punteggio: ");lcd.setCursor(2,1);lcd.print(PunteggioTOT);delay(2000);lcd.clear();
       }
     }
   }
 
 void Giocata2(){
+  Giocata = MetaIn;
+  SelezionaConBottone();
+  MetaIn = Giocata;
     for (int i = 0; i < 4 ; i++) {
       if (Giocata != Caso25 [i]) {
-        lcd.print("Giocata non valida");
+        lcd.print("Giocata non valida");delay(2000);lcd.clear();
       } else {
         lcd.print(Giocata);
         backup = Giocata;
         PunteggioTOT = PunteggioTOT + Giocata;
-        lcd.print("Punteggio: ") + PunteggioTOT;  
+        lcd.print("Punteggio: ");lcd.setCursor(2,1);lcd.print(PunteggioTOT); delay(2000);lcd.clear(); 
       }
     }
   }
-void Giocata3(){
+void Giocata3(){  
+  Giocata = MetaIn;
+  SelezionaConBottone();
+  MetaIn = Giocata;
     for (int i = 0; i < 4 ; i++) {
       if (Giocata != Caso34 [i]) {
-        lcd.print("Giocata non valida");
+        lcd.print("Giocata non valida");delay(2000);lcd.clear();
       } else {
         lcd.print(Giocata);
         backup = Giocata;
         PunteggioTOT = PunteggioTOT + Giocata;
-        lcd.print("Punteggio: ") + PunteggioTOT;
+        lcd.print("Punteggio: ");lcd.setCursor(2,1);lcd.print(PunteggioTOT);delay(2000);lcd.clear();
       }
     }
   }
@@ -127,7 +141,7 @@ void Giocata3(){
 
 
 void ValoreSelezionabile(){ 
-  if (Meta == PunteggioTOT || Meta < PunteggioTOT) {
+  if (Meta == PunteggioTOT || PunteggioTOT > Meta) {
   Stato = 3;}
   else {
        if (Giocata == 0){Giocata0();}
@@ -139,14 +153,6 @@ void ValoreSelezionabile(){
   }
   }
 
-void Gioco() {
-  if(Giocatore == 0){
-  lcd.print("Seleziona un numero da 0 a 6"); 
-  Giocata0();Giocatore = 1;}
-  else if (Giocatore > 0){lcd.print("Seleziona un numero da 1 a 6");
-  lcd.print(Giocata);
-  ValoreSelezionabile();Giocatore++;}
-} 
   
   
 
@@ -162,6 +168,25 @@ void FinePartita()
   }
   Stato = 1;
 }
+
+void Gioco() {
+  lcd.clear();
+  lcd.print("GIOCA");delay(1000);lcd.clear();
+  if(Giocatore == 0)
+  {
+  lcd.print("Seleziona un ");lcd.setCursor(2,1);
+  lcd.print("tra 0 e 6");delay(2000);lcd.clear();
+  } 
+  Giocata0();
+  
+  Giocatore = 1;
+  lcd.clear();
+  lcd.print("GIOCA 2");delay(1000);lcd.clear();
+if (Giocatore > 0){lcd.print("Seleziona un numero ");lcd.setCursor(2,1);
+    lcd.print("tra 1 e 6");delay(2000);lcd.clear();}
+  lcd.print(Giocata);delay(2000);lcd.clear();
+  ValoreSelezionabile();Giocatore++;}
+
 
 
 void loop() {
